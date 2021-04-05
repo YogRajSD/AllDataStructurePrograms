c31350e8852b5a6af4dcd9742fb3a1d096f029a5
Yogesh Rajendra Deolalkar <80164872+YogRajSD@users.noreply.github.com>
2021-04-05 22:35:18 +0530

    Add files via upload
    Circular Linked List implementation in JAVA.

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -30,8 +30,22 @@ public class CircularLinkedList
                 temp = temp.next;  
             toadd.next = head;   
             temp.next = toadd;   
-            head = toadd;  
+            tail = toadd;  
 		}
+	}
+	void addFirst(int x)
+	{
+		Node toadd=new Node(x);
+		toadd.next=head;
+		head=toadd;
+		tail.next=head;
+	}
+	void addLast(int x)
+	{
+		Node toadd=new Node(x);
+		tail.next=toadd;
+		tail=toadd;
+		tail.next=head;
 	}
 	void print()
 	{
@@ -52,6 +66,12 @@ public class CircularLinkedList
 		cll.add(20);
 		cll.add(45);
 		cll.add(464);
+//		cll.print();
+		System.out.println("Adding Node to the First Point");
+		cll.add(23);
+		cll.print();
+		System.out.println("Adding Node to the Last Point");
+		cll.addLast(1000);
 		cll.print();
 
 	}
