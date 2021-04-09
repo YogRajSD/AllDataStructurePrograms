dd35359041c9d8abef6a2d5935180863532b3548
Yogesh Rajendra Deolalkar <80164872+YogRajSD@users.noreply.github.com>
2021-04-09 14:18:38 +0530

    DoublyCircularLinkedList
    Doubly Circular Linked List.

---
+++ DoublyCircularC.java
@@ -0,0 +1,64 @@
+package collections;
+
+public class DoublyCircularC
+{
+	Node head;
+	Node tail;
+	class Node
+	{
+		int data;
+		Node next;
+		Node prev;
+		Node(int data)
+		{
+			this.data=data;
+			next=null;
+		}
+	}
+	// Node created.
+	void create(int data)
+	{
+		Node toadd=new Node(data);
+		if(head==null)
+		{
+			head=toadd;
+			toadd.prev=head;
+			toadd.next=head;
+		}
+		else
+		{
+			Node temp=head;
+			while(temp.next!=head)
+			{
+				temp=temp.next;
+			}
+			temp.next=toadd;
+			toadd.prev=temp;
+			toadd.next=head;
+			head.prev=toadd;
+			tail=toadd;
+		}
+	}
+	void disply()
+	{
+		Node temp=head;
+		while (temp.next!=head) {
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+		System.out.println(temp.data);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DoublyCircularC d=new DoublyCircularC();
+		d.create(87);
+		d.create(72);
+		d.create(70);
+		d.create(75);
+		d.create(74);
+		d.disply();
+
+	}
+
+}
